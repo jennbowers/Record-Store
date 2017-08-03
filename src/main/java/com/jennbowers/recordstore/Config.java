114c062202a9fb0ed763e7cba4a0fbd230ff1f97
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-03 18:53:48 -0400

    Set up files for custom query searches

---
+++ src/main/java/com/jennbowers/recordstore/Config.java
@@ -0,0 +1,6 @@
+package com.jennbowers.recordstore;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+public class Config {}
--- src/main/java/com/jennbowers/recordstore/repositories/AlbumRepository.java
+++ src/main/java/com/jennbowers/recordstore/repositories/AlbumRepository.java
@@ -4,6 +4,9 @@ import com.jennbowers.recordstore.models.Album;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlbumRepository extends CrudRepository<Album, Long>{
+    List<Album> findByName(String name);
 }
