c6f045286361bcc129ce8d8ca84e58665e46c282
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-03 16:42:38 -0400

    Added in main nav bar and created css file

--- src/main/java/com/jennbowers/recordstore/controllers/IndexController.java
+++ src/main/java/com/jennbowers/recordstore/controllers/IndexController.java
@@ -1,13 +1,39 @@
 package com.jennbowers.recordstore.controllers;
 
+import com.jennbowers.recordstore.models.Album;
+import com.jennbowers.recordstore.models.Band;
+import com.jennbowers.recordstore.models.Song;
+import com.jennbowers.recordstore.repositories.AlbumRepository;
+import com.jennbowers.recordstore.repositories.BandRepository;
+import com.jennbowers.recordstore.repositories.SongRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    BandRepository bandRepo;
+
+    @Autowired
+    AlbumRepository albumRepo;
+
+    @Autowired
+    SongRepository songRepo;
+
     @RequestMapping("/")
-    public String index () {
+    public String index (Model model) {
+        Iterable<Band> bands = bandRepo.findAll();
+        model.addAttribute("bands", bands);
+
+        Iterable<Album> albums = albumRepo.findAll();
+        model.addAttribute("albums", albums);
+
+        Iterable<Song> songs = songRepo.findAll();
+        model.addAttribute("songs", songs);
+
         return "index";
     }
 }
