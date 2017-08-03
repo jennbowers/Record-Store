b00a647e881c04093855f1ef9415957eb4a8cf67
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-03 19:09:44 -0400

    Added button on index to link to see all pages.

--- src/main/java/com/jennbowers/recordstore/repositories/SongRepository.java
+++ src/main/java/com/jennbowers/recordstore/repositories/SongRepository.java
@@ -4,6 +4,9 @@ import com.jennbowers.recordstore.models.Song;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SongRepository extends CrudRepository<Song, Long>{
+    List<Song> findByName(String name);
 }
