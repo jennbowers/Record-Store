2c86d49ea3bf19ea5949bc894ccd50667f6f20c3
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-04 11:12:22 -0400

    Refactored controllers.

--- src/main/java/com/jennbowers/recordstore/controllers/AlbumController.java
+++ src/main/java/com/jennbowers/recordstore/controllers/AlbumController.java
@@ -1,4 +1,82 @@
 package com.jennbowers.recordstore.controllers;
 
+import com.jennbowers.recordstore.models.Album;
+import com.jennbowers.recordstore.models.Band;
+import com.jennbowers.recordstore.repositories.AlbumRepository;
+import com.jennbowers.recordstore.repositories.BandRepository;
+import com.jennbowers.recordstore.repositories.SongRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
 public class AlbumController {
+
+    @Autowired
+    BandRepository bandRepo;
+
+    @Autowired
+    AlbumRepository albumRepo;
+
+    @Autowired
+    SongRepository songRepo;
+
+    @RequestMapping(value = "/albums", method = RequestMethod.GET)
+    public String albums (Model model) {
+        Iterable<Album> albums = albumRepo.findAll();
+        model.addAttribute("albums", albums);
+        return "albums";
+    }
+
+    @RequestMapping(value = "/albums", method = RequestMethod.POST)
+    public String albumsSearch (@RequestParam("search") String nameSearch,
+                                Model model) {
+        List<Album> albums = albumRepo.findByName(nameSearch);
+        model.addAttribute("albums", albums);
+        return "albums";
+    }
+
+    @RequestMapping(value = "/albums/add", method = RequestMethod.GET)
+    public String addAlbums (Model model) {
+        Iterable<Band> bands = bandRepo.findAll();
+        model.addAttribute("bands", bands);
+        return "addAlbums";
+    }
+
+    @RequestMapping(value = "/albums/add", method = RequestMethod.POST)
+    public String addAlbumsPost (@RequestParam("band") long bandId,
+                                 @RequestParam("name") String name,
+                                 @RequestParam("genre") String genre,
+                                 @RequestParam("yearReleased") int yearReleased,
+                                 @RequestParam("label") String label,
+                                 @RequestParam("imgUrl") String imgUrl) {
+        Band band = bandRepo.findOne(bandId);
+        Album newAlbum = new Album();
+        newAlbum.setBand(band);
+        newAlbum.setName(name);
+        newAlbum.setGenre(genre);
+        newAlbum.setYearReleased(yearReleased);
+        newAlbum.setLabel(label);
+        newAlbum.setImgUrl(imgUrl);
+        albumRepo.save(newAlbum);
+
+        return "redirect:/albums";
+
+    }
+
+    @RequestMapping(value = "/albums/edit/{albumId}", method = RequestMethod.GET)
+    public String editAlbum (@PathVariable("albumId") long albumId,
+                             Model model) {
+        Album album = albumRepo.findOne(albumId);
+
+        model.addAttribute("album", album);
+        return "editAlbum";
+    }
+
 }
--- src/main/java/com/jennbowers/recordstore/controllers/SongController.java
+++ src/main/java/com/jennbowers/recordstore/controllers/SongController.java
@@ -1,4 +1,89 @@
 package com.jennbowers.recordstore.controllers;
 
+import com.jennbowers.recordstore.models.Album;
+import com.jennbowers.recordstore.models.Band;
+import com.jennbowers.recordstore.models.Song;
+import com.jennbowers.recordstore.repositories.AlbumRepository;
+import com.jennbowers.recordstore.repositories.BandRepository;
+import com.jennbowers.recordstore.repositories.SongRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
 public class SongController {
+
+    @Autowired
+    BandRepository bandRepo;
+
+    @Autowired
+    AlbumRepository albumRepo;
+
+    @Autowired
+    SongRepository songRepo;
+
+    @RequestMapping(value = "/songs", method = RequestMethod.GET)
+    public String songs (Model model) {
+        Iterable<Song> songs = songRepo.findAll();
+        model.addAttribute("songs", songs);
+        return "songs";
+    }
+
+    @RequestMapping(value = "/songs", method = RequestMethod.POST)
+    public String songsSearch (@RequestParam("search") String nameSearch,
+                               Model model) {
+        List<Song> songs = songRepo.findByName(nameSearch);
+        model.addAttribute("songs", songs);
+        return "songs";
+    }
+
+    @RequestMapping(value = "/songs/add", method = RequestMethod.GET)
+    public String addSongs (Model model) {
+        Iterable<Band> bands = bandRepo.findAll();
+        model.addAttribute("bands", bands);
+
+        Iterable<Album> albums = albumRepo.findAll();
+        model.addAttribute("albums", albums);
+
+        return "addSongs";
+    }
+
+    @RequestMapping(value = "/songs/add", method = RequestMethod.POST)
+    public String addSongsPost (@RequestParam("band") long bandId,
+                                @RequestParam("album") long albumId,
+                                @RequestParam("name") String name,
+                                @RequestParam("length") String length) {
+        Band band = bandRepo.findOne(bandId);
+        Album album = albumRepo.findOne(albumId);
+
+        Song newSong = new Song();
+        newSong.setName(name);
+        newSong.setLength(length);
+        newSong.setBand(band);
+        newSong.setAlbum(album);
+
+        songRepo.save(newSong);
+
+        return "redirect:/songs";
+    }
+
+    @RequestMapping(value = "/songs/edit/{songId}", method = RequestMethod.GET)
+    public String editSong (@PathVariable("songId") long songId,
+                            Model model) {
+        Iterable<Band> bands = bandRepo.findAll();
+        model.addAttribute("bands", bands);
+
+        Iterable<Album> albums = albumRepo.findAll();
+        model.addAttribute("albums", albums);
+
+        Song song = songRepo.findOne(songId);
+        model.addAttribute("song", song);
+        return "editSong";
+    }
 }
